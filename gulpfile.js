6c80a6f8957566cc497e87be50f44e415d4ad772
Ravi Ranjan Sharma <rr2436310@gmail.com>
2025-04-21 06:49:07 +0530

    fix gulpfile,npm install sass, gulp runned err i globals

--- gulpfile.js
+++ gulpfile.js
@@ -1,59 +1,60 @@
 // Initialize modules
-const { src, dest, watch, series } = require('gulp');
-const sass = require('gulp-sass');
-const postcss = require('gulp-postcss');
-const autoprefixer = require('autoprefixer');
-const cssnano = require('cssnano');
-const babel = require('gulp-babel');
-const terser = require('gulp-terser');
-const browsersync = require('browser-sync');
+const { src, dest, watch, series } = require("gulp");
+const sass = require("gulp-sass")(require("sass"));
+const postcss = require("gulp-postcss");
+const autoprefixer = require("autoprefixer");
+const cssnano = require("cssnano");
+const babel = require("gulp-babel");
+const terser = require("gulp-terser");
+const browsersync = require("browser-sync");
 
 // Use dart-sass for @use
-sass.compiler = require('dart-sass');
+// sass.compiler = require("dart-sass");
 
 //sass Task
 function scssTask() {
-    return src('app/scss/style.scss', { sourcemaps: true })
-        .pipe(sass())
-        .pipe(postcss([autoprefixer(), cssnano()]))
-        .pipe(dest('dist', { sourcemaps: '.' }));
+  return src("app/scss/style.scss", { sourcemaps: true })
+    .pipe(sass().on("error", sass.logError))
+    .pipe(postcss([autoprefixer(), cssnano()]).on("error", console.error))
+    .pipe(dest("dist", { sourcemaps: "." }));
 }
+
 
 // JavaScript Task
 function jsTask() {
-    return src('app/js/script.js', { sourcemaps: true })
-        .pipe(babel({ presets: ['@babel/preset-env'] }))
-        .pipe(terser())
-        .pipe(dest('dist', { sourcemaps: '.' })); 
+  return src("app/js/script.js", { sourcemaps: true })
+    .pipe(babel({ presets: ["@babel/preset-env"] }))
+    .pipe(terser())
+    .pipe(dest("dist", { sourcemaps: "." }));
 }
 
 // Browsersync
 function browserSyncServer(cb) {
-    browsersync.init({
-        server: {
-            baseDir: '.',
-        },
-        notify: {
-            styles: {
-                top: 'auto',
-                bottom: '0',
-            },
-        },
-    });
-    cb();
+  browsersync.init({
+    server: {
+      baseDir: ".",
+    },
+    notify: {
+      styles: {
+        top: "auto",
+        bottom: "0",
+      },
+    },
+  });
+  cb();
 }
 function BrowserSyncReload(cb) {
-    browsersync.reload();
-    cb();
+  browsersync.reload();
+  cb();
 }
 
 // Watch Task
 function watchTask() {
-    watch('*.html', BrowserSyncReload);
-    watch(
-        ['app/scss/**/*.scss', 'app/**/*.js'],
-        series(scssTask, jsTask, BrowserSyncReload)
-    );
+  watch("*.html", BrowserSyncReload);
+  watch(
+    ["app/scss/**/*.scss", "app/**/*.js"],
+    series(scssTask, jsTask, BrowserSyncReload)
+  );
 }
 
 // Default Gulp Task
